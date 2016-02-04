2dbd0b9fc46877025490f8df01a37230092f151f
soramimi <soramimi@soramimi.jp>
2016-02-04 22:45:52 +0900

    fix for linux

--- RELEASE-RASPI.sh
+++ RELEASE-RASPI.sh
@@ -3,8 +3,8 @@ cd build
 rm -fr SkyMPC
 mkdir SkyMPC
 rm SkyMPC.zip
-cp Release/SkyMPC SkyMPC
+cp Raspberry/SkyMPC SkyMPC
 cp ../SkyMPC_ja.qm SkyMPC
 cp ../SkyMPC.svg SkyMPC
 cp ../LinuxDesktop/SkyMPC.desktop SkyMPC
-tar zcvf SkyMPC-linux.tar.gz SkyMPC
+tar zcvf SkyMPC-raspi.tar.gz SkyMPC
