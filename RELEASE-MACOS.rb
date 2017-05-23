70a5fa03cc8e8cdc1bca58764672d88f3ca3a197
soramimi <fi7s-fct@asahi-net.or.jp>
2017-05-24 00:04:48 +0900

    スクリプト更新

---
+++ RELEASE-MACOS.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+require 'fileutils'
+
+load 'version.rb'
+
+$workdir = "_release"
+
+FileUtils.rm_rf($workdir)
+FileUtils.mkpath($workdir)
+
+`cp -a ../_build_#{$product_name}_Release/#{$product_name}.app #{$workdir}/`
+FileUtils.cp("../_build_AskPass_Release/askpass", "#{$workdir}/#{$product_name}.app/Contents/MacOS/")
+
+`/opt/Qt5.8.0/5.8/clang_64/bin/macdeployqt #{$workdir}/#{$product_name}.app`
+
+Dir.chdir($workdir) {
+        `zip -r #{$product_name}-#{$version_a}.#{$version_b}.#{$version_c}-macos.zip #{$product_name}.app`
+}
+
--- RELEASE-MACOSX.sh
+++
@@ -1,4 +0,0 @@
-cd build
-cp -a Release/SkyMPC.app .
-/opt/Qt5.6.0/5.6/clang_64/bin/macdeployqt SkyMPC.app
-tar zcvf SkyMPC.zip SkyMPC.app
