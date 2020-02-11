1ba975ab3e208a32376399047a8dbb2f5996cb7f
soramimi <fi7s-fct@asahi-net.or.jp>
2020-02-11 22:46:51 +0900

    HighDPI対応

--- src/main.cpp
+++ src/main.cpp
@@ -60,6 +60,7 @@ int main(int argc, char *argv[])
 	}
 
 	QApplication a(argc, argv);
+	a.setAttribute(Qt::AA_UseHighDpiPixmaps);
 
 	if (QApplication::queryKeyboardModifiers() & Qt::ShiftModifier) {
 		global->start_with_shift_key = true;
